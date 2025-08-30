b74e166dc7a0ef05c40621ac6774d24ab39a7ae7
mohantommy <150316366+mohantommy@users.noreply.github.com>
2025-08-30 14:20:35 +0530

    Added MySQL data cleaning script

---
+++ data cleaning in mySQL.sql
@@ -0,0 +1,103 @@
+SELECT *
+FROM layoffs;
+
+-- 1.remove duplicates
+-- 2.standardize the data
+-- 3.Null values or Blank values
+-- 4.remove any coloumns
+
+DROP TABLE IF EXISTS layoffs_staging2;
+
+CREATE TABLE layoffs_staging2 AS
+SELECT *,
+ROW_NUMBER() OVER(
+  PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, date, stage, country, funds_raised_millions
+  ORDER BY company
+) AS row_num
+FROM layoffs_staging;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+DELETE 
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT *
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+DELETE 
+FROM layoffs_staging2
+WHERE row_num > 1;
+
+SELECT company, TRIM(company)
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET company = TRIM(company);
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging2
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT country, TRIM(TRAILING '.' FROM country)
+FROM layoffs_staging2
+ORDER BY 1;
+
+UPDATE layoffs_staging2
+SET country = TRIM(TRAILING '.' FROM country)
+WHERE country = 'United States%';
+
+
+SELECT `date`
+FROM layoffs_staging2;
+
+UPDATE layoffs_staging2
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+ALTER TABLE layoffs_staging2 
+MODIFY COLUMN `date` DATE;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE company = 'Airbnb';
+
+UPDATE layoffs_staging2
+SET industry = NULL
+WHERE industry = '';
+
+SELECT t1.industry, t2.industry
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+    AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL; 
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL 
+AND t2.industry IS NOT NULL; 
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+SELECT *
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
